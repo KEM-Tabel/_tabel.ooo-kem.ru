914c1909958aa10613d06ee91dc1f1dc6eeb3c01
Nikita Bezbabnov <82368263+SwetoFill@users.noreply.github.com>
2025-05-27 10:16:50 +1000

    Update report.js

--- js/report.js
+++ js/report.js
@@ -31,7 +31,7 @@ let unselectedBgd	= '#f6f7f6';
 let unselectedNoBgd	= '#e7e77e';
 let todayBgd		= '#ff0000';
 let weekendBgd		= 'repeating-linear-gradient(-45deg, #d7db00 0px, #d7db00 2px, #ddd 2px, #ddd 5px)';
-let VER 			= "21";
+let VER 			= "22";
 
 let TIMESTAMP_SESSION	= Math.floor(Date.now() / 1000);
 let TIMESTAMP_ACTIVITY 	= Math.floor(Date.now() / 1000);
